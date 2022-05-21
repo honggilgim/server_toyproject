69b211f17c8dcefebf7e4addd65644e06550a8ff
honggilgim <ghdrlf0911@naver.com>
2022-05-21 23:39:29 +0900

    react-native 초기 모델 연동 react native 공부 따로 해야 될거 같음

--- nativetest/App.js
+++ nativetest/App.js
@@ -1,112 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import 'package:flutter/material.dart';
+import 'dart:async';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import 'package:flutter/services.dart';
+import 'package:bootpay_api/bootpay_api.dart';
+import 'package:bootpay_api/model/payload.dart';
+import 'package:bootpay_api/model/extra.dart';
+import 'package:bootpay_api/model/user.dart';
+import 'package:bootpay_api/model/item.dart';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+void main() => runApp(MyApp());
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Test",
+      home: TestPage(),
+    );
+  }
+}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+class TestPage extends StatefulWidget {
+  @override
+  TestPageState createState() => TestPageState();
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+class TestPageState extends State<TestPage> {
+//  String _platformVersion = 'Unknown';
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  @override
+  void initState() {
+    super.initState();
+//    initPlatformState();
+  }
+ 
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Container(
+          child:  RaisedButton(
+            onPressed: () {
+              goBootpayRequest(context);
+            },
+            child: Text("부트페이 결제요청"),
+          ),
+        )
+    );
+  }
 
-export default App;
+  static Future<void> request(BuildContext context, Payload payload,
+      {User user,
+      List<Item> items,
+      Extra extra,
+      StringCallback onDone,
+      StringCallback onReady,
+      StringCallback onCancel,
+      StringCallback onError}) async {
+
+    payload.applicationId = Platform.isIOS
+        ? payload.iosApplicationId
+        : payload.androidApplicationId;
+
+    if (user == null) user = User();
+    if (items == null) items = [];
+    if (extra == null) extra = Extra();
+
+    Map<String, dynamic> params = {
+      "payload": payload.toJson(),
+      "user": user.toJson(),
+      "items": items.map((v) => v.toJson()).toList(),
+      "extra": extra.toJson()
+    };
+
+
+    Map<dynamic, dynamic> result = await _channel.invokeMethod(
+      "bootpayRequest",
+      params,
+    );
+
+
+    String method = result["method"];
+    if (method == null) method = result["action"];
+
+    String message = result["message"];
+    if (message == null) message = result["msg"];
+
+
+
+    //confirm 생략
+    if (method == 'onDone' || method == 'BootpayDone') {
+      if (onDone != null) onDone(message);
+    } else if (method == 'onReady' || method == 'BootpayReady') {
+      if (onReady != null) onReady(message);
+    } else if (method == 'onCancel' || method == 'BootpayCancel') {
+      if (onCancel != null) onCancel(message);
+    } else if (method == 'onError' || method == 'BootpayError') {
+      if (onError != null) onError(message);
+    } else if (result['receipt_id'] != null && result['receipt_id'].isNotEmpty) {
+      if (onDone != null) onDone(jsonEncode(result));
+    }
+  }
+}
